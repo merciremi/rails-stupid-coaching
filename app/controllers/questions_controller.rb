5bf2dae3c345b021c62865e4629b0fd77f85b110
Remi Mercier <remi.merci@gmail.com>
2018-05-14 12:22:47 +0200

    Style question and answer in anwser view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,9 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+
+    @question = params[:question]
+
     if params[:question] == 'I am going to work'
       @answer = 'Great!'
     elsif params[:question].include? '?'
